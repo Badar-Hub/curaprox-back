39dd4027ccb5eadf58e4b4d4b565bb729389875e
Hamza <monosoft54@gmail.com>
2020-10-02 17:03:38 +0500

    Products CRUD Integrated

--- DAL/models/product.model.js
+++ DAL/models/product.model.js
@@ -1,27 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: 'Required'
-    },
-    sku: {
-        type: String,
-        required: 'Required'
-    },
-    qty: {
-        type: Number,
-        required: 'Required'
-    },
-    description: {
-        type: String
-    },
-    price: {
-        type: Number,
-        required: 'Required'
-    },
-})
+  name: {
+    type: String,
+    required: "Required",
+  },
+  sku: {
+    type: String,
+    required: "Required",
+  },
+  qty: {
+    type: Number,
+    required: "Required",
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: "Required",
+  },
+  img: {
+    type: String,
+    required: "Required",
+  },
+});
 
-const model = mongoose.model('product', productSchema, 'products');
+const model = mongoose.model("product", productSchema, "products");
 
 module.exports = model;
--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const app = express();
-let port = 80;
+var fs = require("fs");
+var formidable = require("formidable");
 const connection = require("./DAL/connection");
 const ProductModel = require("./DAL/models/product.model");
 const serveStatic = require("serve-static");
 // parse application/x-www-form-urlencoded
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-);
+const app = express();
+let port = 80;
+
+var multipart = require("connect-multiparty");
+var multipartMiddleware = multipart();
+
+// replace this with the location to save uploaded files
+var upload_path = __dirname + "/admin/files/prod-images/";
+
+// express.static("/admin/files");
 
 // parse application/json
+app.use("/", serveStatic(__dirname + "/dist"));
+app.use("/admin", serveStatic(__dirname + "/dist-back"));
+app.use("/admin/files", serveStatic(__dirname + "/admin/files"));
 app.use(bodyParser.json());
-app.use('/', serveStatic(__dirname + "/dist"));
-app.use('/admin', serveStatic(__dirname + "/dist-back"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/api/products", (req, res) => {
   ProductModel.find((err, data) => {
@@ -23,13 +34,30 @@ app.get("/api/products", (req, res) => {
   });
 });
 
-app.post("/api/product", (req, res) => {
+app.get("/api/product/:id", (req, res) => {
+  ProductModel.findById(req.params.id, (err, data) => {
+    if (!err) res.json(data);
+  });
+});
+
+app.post("/api/product", multipartMiddleware, (req, res) => {
   let product = new ProductModel(req.body);
   console.log(req.body);
-  product.save((err, data) => {
-    if (err) res.json(err.message);
-    res.json(data);
-  });
+  let file = req.files.file;
+  console.log(file);
+  if (file) {
+    var oldpath = file.path;
+
+    var newpath = upload_path + file.name;
+    fs.rename(oldpath, newpath, function (err) {
+      if (err) throw err;
+      // you may respond with another html page
+      product.img = "files/prod-images/" + file.name;
+      product.save((err, data) => {
+        if (!err) res.json(data);
+      });
+    });
+  }
 });
 
 app.listen(port, () => {
